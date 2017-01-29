7d9193d2fbc1235b18598117c9066e91291719d2
vincentpisani <Turtle1331@yandex.com>
2017-01-28 16:43:06 -0800

    New Blinkt floodlight control, added .idea to .gitignore

---
+++ floodlight_listener.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import json
+import logging
+from logging import info
+import time
+
+import blinkt
+
+from common_constants import LOGGING_ARGS
+from common_utils import mqtt_broker_info
+from common_utils import sleep
+from mqtt_connection import MqttConnection
+
+HOSTNAME = "hostname"
+PORT = "port"
+
+
+def on_connect(client, userdata, flags, rc):
+    info("{0} connecting to {1}:{2}".format("Success" if rc == 0 else "Failure", userdata[HOSTNAME], userdata[PORT]))
+    client.subscribe("#")
+
+
+def on_disconnect(client, userdata, rc):
+    info("Disconnected with result code: {0}".format(rc))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    info("Subscribed with message id: {0} QOS: {1}".format(mid, granted_qos))
+
+
+def on_message(client, userdata, msg):
+    # Decode json object payload
+    json_val = json.loads(bytes.decode(msg.payload))
+    info("{0} : {1}".format(msg.topic, json_val))
+    global color, duration, duty_cycle, intensity
+    color = json_val["color"]
+    duration = json_val["duration"]
+    duty_cycle = json_val["duty_cycle"]
+    intensity = json_val["intensity"]
+
+
+def run_display():
+    global color, duration, duty_cycle, intensity
+    while True:
+        for n in range(8):
+            blinkt.set_pixel(n, color["r"], color["g"], color["b"], intensity)
+        blinkt.show()
+        time.sleep(duration * duty_cycle)
+        for n in range(8):
+            blinkt.set_pixel(n, 0, 0, 0, intensity)
+        blinkt.show()
+        time.sleep(duration * (1 - duty_cycle))
+
+
+if __name__ == "__main__":
+    # Parse CLI args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mqtt", required=True, help="MQTT broker hostname")
+    args = vars(parser.parse_args())
+
+    # Setup logging
+    logging.basicConfig(**LOGGING_ARGS)
+
+    # Create MQTT connection
+    hostname, port = mqtt_broker_info(args["mqtt"])
+    mqtt_conn = MqttConnection(hostname, port, userdata={HOSTNAME: hostname, PORT: port})
+    mqtt_conn.client.on_connect = on_connect
+    mqtt_conn.client.on_disconnect = on_disconnect
+    mqtt_conn.client.on_subscribe = on_subscribe
+    mqtt_conn.client.on_message = on_message
+    mqtt_conn.connect()
+
+    try:
+        run_display()
+    except KeyboardInterrupt:
+        pass
+
+    print("Exiting...")
