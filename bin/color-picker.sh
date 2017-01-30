44dd94b34aee82ca2403462104d86ba9face6d22
Paul Ambrose <pambrose@mac.com>
2017-01-29 18:17:20 -0800

    Add scripts to tun on turtlenet after reboot

--- bin/color-picker.sh
+++ bin/color-picker.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-source /home/pi/start_py2cv3.sh
+echo "Starting Python 2.7 with OpenCV 3.1 bindings..."
+source /home/pi/.profile
+workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:/home/pi/git/common-robotics:/home/pi/git/object-tracking
 /home/pi/git/object-tracking/color_picker.py -w 600
--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
-source /home/pi/start_py2cv3.sh
+echo "Starting Python 2.7 with OpenCV 3.1 bindings..."
+source /home/pi/.profile
+workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:/home/pi/git/common-robotics:/home/pi/git/object-tracking
 /home/pi/git/object-tracking/object_tracker.py --bgr "174, 56, 5" --width 400 --flip
