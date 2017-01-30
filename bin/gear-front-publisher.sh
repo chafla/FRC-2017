1e85cd32714dd4797e213474e406e817192fe0ae
Paul Ambrose <pambrose@mac.com>
2017-01-29 22:55:35 -0800

    Add scripts to tun on turtlenet after reboot

--- bin/gear-front-publisher.sh
+++ bin/gear-front-publisher.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+echo "Gear front publisher..."
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 ~pi/git/FRC-2017/location_publisher.py --grpc raspi11.local --camera camera-gear-front --mqtt mqtt-turtle.local
 
