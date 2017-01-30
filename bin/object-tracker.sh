6a395a39c608cd98d14f96de055efe5fbb0388f1
Paul Ambrose <pambrose@mac.com>
2017-01-30 10:06:04 -0800

    Updte name of object_tracker code

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -3,4 +3,4 @@
 source ~pi/.profile
 workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
-python2 ~pi/git/object-tracking/object_tracker.py --bgr "174, 56, 5" --width 400 --flip &> ~pi/git/FRC-2017/logs/object-tracker.out &
+python2 ~pi/git/object-tracking/single_object_tracker.py --bgr "174, 56, 5" --width 400 --flip &> ~pi/git/FRC-2017/logs/object-tracker.out &
