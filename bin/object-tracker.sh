f14526a637576c1f86dbe2034d450fc8d6132f87
Paul Ambrose <pambrose@mac.com>
2017-01-29 22:36:46 -0800

    Add scripts to tun on turtlenet after reboot

--- bin/object-tracker.sh
+++ bin/object-tracker.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-echo "Starting Python 2.7 with OpenCV 3.1 bindings..."
+echo "Starting Object tracker..."
 source /home/pi/.profile
 workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:/home/pi/git/common-robotics:/home/pi/git/object-tracking
 /home/pi/git/object-tracking/object_tracker.py --bgr "174, 56, 5" --width 400 --flip
+echo "Started Object tracker..."
