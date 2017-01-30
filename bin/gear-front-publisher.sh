85ff0f9790da708d1186cbc1cc3c9465eaf83c99
Paul Ambrose <pambrose@mac.com>
2017-01-29 23:58:30 -0800

    Use cv2 virtualenv on all scripts

--- bin/gear-front-publisher.sh
+++ bin/gear-front-publisher.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+source /home/pi/.profile
+workon py2cv3
 export PYTHONPATH=${PYTHONPATH}:~pi/git/common-robotics:~pi/git/object-tracking
 python2 ~pi/git/FRC-2017/location_publisher.py --grpc raspi11.local --camera camera-gear-front --mqtt mqtt-turtle.local &> ~pi/git/FRC-2017/logs/gear-front-publisher.out &
 
