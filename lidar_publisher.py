9027bcedda722305866f0f77f7bb15244c69dd02
chafla <namen937@gmail.com>
2017-02-01 04:38:18 +0000

    Implementing threading

--- lidar_publisher.py
+++ lidar_publisher.py
@@ -6,13 +6,18 @@ import socket
 import time
 from threading import Thread
 import serial
-from sys import exit, stderr
+from sys import stderr, exit
 
 import paho.mqtt.client as paho
-# from common_constants import LOGGING_ARGS
-# from common_utils import mqtt_broker_info
-# from common_utils import is_windows
+from common_constants import LOGGING_ARGS
+from common_utils import mqtt_broker_info
+from common_utils import is_windows
 
+global last_fetched
+global raw_mm
+
+
+'''
 LOGGING_ARGS = {"stream": stderr,
                 "level": logging.INFO,
                 "format": "%(asctime)s %(name)-10s %(funcName)-10s():%(lineno)i: %(levelname)-6s %(message)s"}
@@ -21,7 +26,7 @@ LOGGING_ARGS = {"stream": stderr,
 def mqtt_broker_info(val):
     # Broker hostname can be either "localhost" or "localhost:999"
     return (val[:val.index(":")], int(val[val.index(":") + 1:])) if ":" in val else (val, 1883)
-
+'''
 
 CLIENT = "client"
 
@@ -30,7 +35,9 @@ TOLERANCE_THRESH = 15
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: {0}".format(rc))
-    Thread(target=publish_messages, args=(client, userdata)).start()
+    # TODO: Work on syncronization tomorrow
+    Thread(target=fetch_data, args=(client, userdata)).start()
+    Thread(target=get_data_from_serial).start()
 
 
 def on_disconnect(client, userdata, rc):
@@ -41,48 +48,60 @@ def on_publish(client, userdata, mid):
     print("Published value to {0} with message id {1}".format(userdata["topic"], mid))
 
 
-def publish_messages(client, userdata):
-    total_sum = 0
-    total_count = 0
+def get_data_from_serial():
+    global raw_mm
 
-    port = userdata["port"]
-
-    # Init connection to serial port
     try:
         ser = serial.Serial(port=port, baudrate=115200)
     except serial.serialutil.SerialException as e:
-        raise e
+        print(e.__class__.__name__, e)
+        ser = None  # Thanks pycharm
+        exit(0)
 
-    try:
-        while True:
-            try:
-                bytes = ser.readline()
-                s = bytes.decode("utf-8")
-                mm = int(s)
-                print(mm)
-                if mm < 0:  # out of range, get fresh data so it doesn't mess with averages
-                    total_sum = 0
-                    total_count = 0
-                    # continue
-                elif (total_sum + total_count == 0) or abs((total_sum / total_count) - mm) < TOLERANCE_THRESH:
-                    total_sum += mm
-                    total_count += 1
-                else:
-                    mm_enc = str(mm).encode("utf-8")
-                    client.publish("{}/mm".format(userdata["topic"]),
-                                   payload=mm_enc,
-                                   qos=0)
-                    total_sum = 0 + mm
-                    total_count = 1
+    while True:
+        try:
+            bytes = ser.readline()
+            raw_mm = int(bytes.decode("utf-8"))
 
-                time.sleep(0.5)
+        except BaseException as e:
+            print(e.__class__.__name__, e)
+            time.sleep(1)
 
-            except BaseException as e:
-                print(e.__class__.__name__, e)
-                time.sleep(1)
 
-    finally:
-        ser.close()
+def fetch_data(client, userdata):
+    total_sum = 0
+    total_count = 0
+    global raw_mm
+
+    while True:
+        try:
+            if raw_mm is None:
+                continue
+
+            mm = raw_mm.copy()
+            if mm < 0:  # out of range, get fresh data so it doesn't mess with averages
+                total_sum = 0
+                total_count = 0
+                # continue
+            elif (total_sum + total_count == 0) or abs((total_sum / total_count) - mm) < TOLERANCE_THRESH:
+                total_sum += mm
+                total_count += 1
+            else:
+                mm_enc = str(mm).encode("utf-8")
+
+                client.publish("{}/mm".format(userdata["topic"]),
+                               payload=mm_enc,
+                               qos=0)
+                raw_mm = None
+
+                total_sum = 0 + mm
+                total_count = 1
+
+            # time.sleep(0.25)  # Needs to be quicker than the update time of the lidar itself
+
+        except BaseException as e:
+            print(e.__class__.__name__, e)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -93,7 +112,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--device", required=True, help="Device name (lidar_l or lidar_r")
     args = vars(parser.parse_args())
 
-    port = "/dev/" + args["serial"]
+    port = ("/dev/" if not is_windows() else "") + args["serial"]
 
     # Init connection to serial port
 
